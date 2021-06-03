cf0924ed0954818a0ebc604e7df660b1df2e5b24
Ibrahim Khalikov <starlink050@gmail.com>
2021-06-03 08:57:17 +0500

    Initial Commit

---
+++ navigationLink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  navigationLink
+//
+//  Created by cloud_vfx on 03/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ navigationLink/navigationLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationLinkApp.swift
+//  navigationLink
+//
+//  Created by cloud_vfx on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct navigationLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
